c9f4a54b4d1f74a7166fb2eb3e89160ea999c220
Daun Kim <yennakim11@gmail.com>
2023-11-19 22:28:52 -0600

    edited package.json and set up

--- public/main.js
+++ public/main.js
@@ -5,10 +5,8 @@ import '../styles/main.scss';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
+    <h1>Joke Generator</h1>
+    <button class="btn btn-secondary" id="click-me">Get Joke</button>
     <h2>These are font awesome icons:</h2>
     <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
   `;
