3fb244794232e018d031afcca570d7b9f7ed6703
Daun Kim <yennakim11@gmail.com>
2023-11-20 20:46:11 -0600

    added promise and render functionality

--- public/main.js
+++ public/main.js
@@ -2,19 +2,27 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getJoke from '../api/promises';
+import renderToDom from '../utils/renderToDom';
 
 const init = () => {
   document.querySelector('#app').innerHTML = `
     <h1>Joke Generator</h1>
-    <button class="btn btn-secondary" id="click-me">Get Joke</button>
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
+    <button class="btn btn-secondary" id="get-joke">Get A Joke!</button>
+    <div id="joke-setup"></div>
+    <div id="joke-punchline"></div>`;
   console.warn('YOU ARE UP AND RUNNING!');
 
   document
-    .querySelector('#click-me')
+    .querySelector('#get-joke')
     .addEventListener('click', () => console.warn('You clicked that button!'));
+
+  document.querySelector('#get-joke').addEventListener('click', () => {
+    getJoke().then((data) => {
+      renderToDom('#joke-setup', data.setup);
+      renderToDom('#joke-punchline', data.delivery);
+    });
+  });
 
   // USE WITH FIREBASE AUTH
   // ViewDirectorBasedOnUserAuthStatus();
